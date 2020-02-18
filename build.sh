65d772f98abeb2c2d7b4db7c165af7db0e584fdc
Sergey Bykov <esphynox@macpaw.com>
2020-02-18 23:46:49 +0200

    Removed unused tools & updated build script

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 applecommander -dos140 exit.dsk; # create image disk with dosk image
-cat exit.bas | applecommander -bas exit.dsk exit; # put 3d.bas in exit.dsk
+cat exit.bas | applecommander -bas exit.dsk exit; # put exit.bas in exit.dsk
 
 images=("splash" "1-1" "1-2" "1-3" "1-4" "1-5" "1-6" "1-7" "2-1")
 for i in "${images[@]}"
 do
-  # ./png2hgr images/png_indexed/${i}_280x192_indexed.png > images/hgr/${i}.hgr # convert png to hgr v
-  ./tohgr images/png/${i}_280x192.png # convert png to hgr w/ tohgr
-  mv `pwd`/images/png/${i}_280x192.hgr `pwd`/images/hgr/${i}.hgr # move .hgr file to 'hgr' folder
+  ./tohgr images/png/${i}_280x192.png # convert png to hgr w/ `tohgr` tool
+  mv `pwd`/images/png/${i}_280x192.hgr `pwd`/images/hgr/${i}.hgr # move .hgr file to "hgr" folder
   rm `pwd`/images/png/${i}_280x192_hgr.png # remove .png preview file
   applecommander -p exit.dsk ${i}.HGR data < images/hgr/${i}.HGR; # put hgr to disk image
 done
