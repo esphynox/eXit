a50b1e323926cde227eff942a43164ac646ad7a9
Sergey Bykov <esphynox@macpaw.com>
2020-02-19 22:17:21 +0200

    Images renamed

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cp empty.dsk exit.dsk # copy pre-existing .dsk image
 cat src/hello.bas | applecommander -bas exit.dsk hello; # put hello.bas in exit.dsk
 cat src/exit.bas | applecommander -bas exit.dsk exit; # put exit.bas in exit.dsk
 
-images=("splash" "1-1" "1-2" "1-3" "1-4" "1-5" "1-6" "1-7" "2-1")
+images=("img0" "img1" "img2" "img3" "img4" "img5" "img6" "img7" "img8")
 for i in "${images[@]}"
 do
   ./bin/tohgr images/png/${i}_280x192.png # convert png to hgr w/ `tohgr` tool
