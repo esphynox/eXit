c34cc04d6970df8873d52a2878ed730c2af16405
Serhii Bykov <s.bykov@kasta.ua>
2020-02-28 00:27:26 +0200

    Build script cleared from absolute paths

--- build.sh
+++ build.sh
@@ -11,10 +11,10 @@ images=("img0" "img1" "img2" "img3" "img4" "img5" "img6" "img7" "img8")
 for i in "${images[@]}"
 do
   ./bin/tohgr images/png/${i}_280x192.png # convert png to hgr w/ `tohgr` tool
-  mv `pwd`/images/png/${i}_280x192.hgr `pwd`/images/hgr/${i}.hgr # move .hgr file to "hgr" folder
-  rm `pwd`/images/png/${i}_280x192_hgr.png # remove .png preview file
+  mv images/png/${i}_280x192.hgr images/hgr/${i}.hgr # move .hgr file to "hgr" folder
+  rm images/png/${i}_280x192_hgr.png # remove .png preview file
   applecommander -p exit.dsk ${i}.HGR data < images/hgr/${i}.HGR; # put hgr to disk image
 done
 
 # copy disk to microm8 folder
-cp /Users/esphynox/Work/My/eXit/exit.dsk /Users/esphynox/microM8/MyDisks/exit.dsk;  
+cp exit.dsk ~/microM8/MyDisks/exit.dsk 
